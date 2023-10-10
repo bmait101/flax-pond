116a452313662c0527e17a2351c67262600ba70e
Bryan M <bmaitland101@gmail.com>
2023-10-10 17:30:11 -0500

    new lit table and code

--- R/01_data.R
+++ R/01_data.R
@@ -13,22 +13,24 @@ species <- read_csv(here("data", "spp-codes.csv")) |> janitor::clean_names()
 # clean and tidy
 flax <- flax |> 
   left_join(species, by = "spp_code") |> 
-  select(year, date, time, d_n, location, depth, area, spp_code, common_name, total_caught, mean_weight)
+  select(year, date, time, d_n, location, depth, area, spp_code, common_name, total_caught, mean_weight) |> 
+  mutate(density = mean_weight/area)
 
 # Simple plot to look at things
 flax |> 
-  ggplot(aes(date, total_caught)) + 
-  geom_point() + 
-  facet_wrap(vars(common_name))
-
-
-# flounder density by site
-flax |> 
-  mutate(density = mean_weight/area) |> 
-  filter(common_name=="Flounder") |> 
   ggplot(aes(date, density)) + 
   geom_point() + 
-  facet_wrap(vars(location))
+  facet_wrap(vars(common_name), scales = "free_y")
 
 
+# flounder density
+flax |> 
+  # filter(common_name=="Flounder",year!="72 (2nd)",year!="72 (3rd)") |>
+  mutate(month = month(date, label = TRUE)) |> 
+  ggplot(aes(month, density)) + 
+  geom_bar(stat = "identity") + 
+  # facet_wrap(vars(year), scales = "free") +
+  geom_smooth(se = FALSE)
 
+
+plot(flax$area ~ flax$depth)
